d9b52e250305630cf96f9871b51c1aa3af8b0c6e
Michael Mansperger <ManspergerMichael@gmail.com>
2018-11-29 18:53:28 -0800

    balence point and balance index

---
+++ Balancepoint/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Balancepoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] arr = {1,2,3,4,10};
+            int[] arr2 = {10,4,3,2,1};
+            int[] arr3 = {1,2,4,2,1};
+            int[] arr4 = {-2,5,7,0,3};
+            //System.Console.WriteLine(Balancepoint(arr3));
+            System.Console.WriteLine(balanceIndex(arr4));
+        }
+
+        public static bool Balancepoint(int[] arr){
+            if(arr.Length <= 1){
+                return false;
+            }
+            int right = 0;
+            int left = 0;
+            for(int i = 0; i < arr.Length; i ++){
+                right += arr[i];
+                for(int j = arr.Length -1; j > i; j --){
+                    left += arr[j];
+                    
+                }
+                if(right == left){
+                    System.Console.WriteLine(i);
+                    return true;
+                }
+                else{
+                    left = 0; 
+                }
+                             
+            }
+            return false;
+        }
+
+        
+    public static int balanceIndex(int[] arr){
+        if(arr.Length < 3){
+            return -1;
+        }
+        int left = 0;
+        int right = 0;
+        for(int i = 0; i < arr.Length; i++){
+            //left sum
+            for(int j = 0; j < i; j++){
+                left += arr[j];
+            }
+            //right sum
+            for(int j = i + 1; j < arr.Length; j++){
+                right += arr[j];
+            }
+            if(right == left){
+                return i;
+            }
+            else{
+                left = 0;
+                right = 0;
+            }
+        }
+        return -1;
+    }
+
+    }
+
+}
