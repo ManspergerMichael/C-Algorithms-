0dc4c74a83be2a74044e55a0779bfa6e21f35a33
Michael Mansperger <ManspergerMichael@gmail.com>
2018-12-15 17:04:17 -0800

    Binary search tree
    Started practicing Binary search trees. and other updates not listed here.

---
+++ sequentialDigits/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sequentialDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            sequentialDigits(10,30);
+        }
+        static void sequentialDigits(int lowPoint, int highPoint){
+            bool flag = true;
+            List<int> result = new List<int>();
+            
+            for(int i = lowPoint; i < highPoint; i++){
+                int test = 0;
+                flag = true;
+                int num = i;
+                //check each digit of the number
+                while(num > 0){
+                    test = num % 10;
+                    num = num / 10;
+                    System.Console.WriteLine("test: "+ test+ "num: " + num);
+                    if(num % 10 != test - 1){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag == true){
+                    //System.Console.WriteLine(i);
+                    result.Add(i);
+                }
+            }
+            foreach (var item in result)
+            {
+                System.Console.WriteLine(item);
+                
+            }
+            
+        }
+    }
+}
