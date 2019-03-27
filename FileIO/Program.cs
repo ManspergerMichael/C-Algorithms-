56187b2c2c29c6a9c3b649d76021f25dcfb44113
Michael Mansperger <ManspergerMichael@gmail.com>
2019-03-26 19:34:41 -0700

    Started File IO test

---
+++ FileIO/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            ReadFile("./TestFile.csv", 2);
+        }
+        public static void ReadFile(String fileName, int column)
+        {
+            try
+            {
+                String st = System.IO.File.ReadAllText(fileName);
+                //System.Console.WriteLine(st);
+            }
+            catch( Exception e )
+            {
+                System.Console.WriteLine("The File Could Not Be Read");
+                System.Console.WriteLine(e.Message);
+            }
+            String file = System.IO.File.ReadAllText(fileName);
+            using (StringReader reader = new StringReader(file))
+            {
+                String line;
+                line = reader.ReadLine();
+                while((line = reader.ReadLine()) != null)
+                {
+                    if(column == 1)
+                    {
+                        System.Console.WriteLine(line.Substring(0,1));
+                    }
+                    if(column == 2)
+                    {
+                        System.Console.WriteLine(line.Substring(2,1));
+                    }
+                    
+                }
+            }
+        }
+    }
+}
