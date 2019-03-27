c90ddc89af405c4014630bf5d955790faf2e3b33
Michael Mansperger <ManspergerMichael@gmail.com>
2019-03-26 19:50:06 -0700

    moar

--- FileIO/Program.cs
+++ FileIO/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 namespace FileIO
 {
@@ -11,6 +12,7 @@ namespace FileIO
         }
         public static void ReadFile(String fileName, int column)
         {
+            Dictionary<String,int> dict = new Dictionary<String,int>();
             try
             {
                 String st = System.IO.File.ReadAllText(fileName);
@@ -26,18 +28,38 @@ namespace FileIO
             {
                 String line;
                 line = reader.ReadLine();
+                
                 while((line = reader.ReadLine()) != null)
                 {
                     if(column == 1)
                     {
-                        System.Console.WriteLine(line.Substring(0,1));
+                        if(dict.ContainsKey(line.Substring(0,1)))
+                        {
+                            dict[line.Substring(0,1)] += 1;
+                        }
+                        else{
+                            dict.Add(line.Substring(0,1), 1);
+                        }
+                        //System.Console.WriteLine(line.Substring(0,1));
                     }
                     if(column == 2)
                     {
+                        if(dict.ContainsKey(line.Substring(2,0)))
+                        {
+                            dict[line.Substring(2,0)] += 1;
+                        }
+                        else{
+                            dict.Add(line.Substring(2,0), 1);
+                        }
                         System.Console.WriteLine(line.Substring(2,1));
                     }
                     
                 }
+
+            }
+            foreach(KeyValuePair<String,int> item in dict)
+            {
+                System.Console.WriteLine(item.Key + " " + item.Value);
             }
         }
     }
